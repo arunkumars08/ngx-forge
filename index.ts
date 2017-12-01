d440218b03bc7b370b3885deab009bb042c8fc45
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-12-01 14:08:37 +0530

    more change

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 
-export { AsciidocComponent } from './src/app/components/asciidoc/asciidoc.component';
 export { AsciidocIndex } from './src/app/components/asciidoc/asciidoc.index';
 export { AsciidocService } from './src/app/components/asciidoc/asciidoc.service';
 export { InputComponent } from './src/app/components/input/input.component';
 export { ProjectSelect, ProjectSelectConfig } from './src/app/components/project-select/project-select.component';
+// export { ForgeErrorsComponent } from './src/app/components/forge-errors/forge-errors.component';
+// export { ForgeExceptionComponent } from './src/app/components/forge-exception/forge-exception.component';
+export { SpinnerComponent } from './src/app/components/spinner/spinner.component';
+export { SingleSelectionDropDownComponent } from
+'./src/app/components/singleselectiondropdown/singleselectiondropdown.component';
+export { MultipleSelectionListComponent } from './src/app/components/multiple-selection-list/multiple-selection-list.component';
+export { SelectedItemsPipe } from './src/app/components/multiple-selection-list/selected-items.pipe';
+export { VisibleItemsPipe } from './src/app/components/multiple-selection-list/visible-items.pipe';
 export * from './src/app/model/base.model';
 export { Config } from './src/app/service/config.component';
 export { ForgeService } from './src/app/service/forge.service';
 export { History } from './src/app/service/history.component';
 export { NgxForgeModule } from './ngx-forge.module';
 export { TokenProvider } from './src/app/service/token-provider';
+
--- ngx-forge.module.ts
+++ ngx-forge.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { AsciidocComponent } from './src/app/components/asciidoc/asciidoc.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './src/app/components/input/input.component';
 import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
 import { ForgeErrorsComponent } from './src/app/components/forge-errors/forge-errors.component';
 import { ForgeExceptionComponent } from './src/app/components/forge-exception/forge-exception.component';
-import { MultipleSelectionListComponent } from './src/app/components/multiple-selection-list/multiple-selection-list.component';
-import { SingleSelectionDropDownComponent } from './src/app/components/single-selection-dropdown/single-selection-dropdown.component';
-import { SpinnerComponent } from './src/app/components/spinner/spinner.component';
+
+// import { SpinnerComponent } from './src/app/components/spinner/spinner.component';
 import { ProjectSelect } from './src/app/components/project-select/project-select.component';
 import { Config } from './src/app/service/config.component';
 import { ForgeService } from './src/app/service/forge.service';
@@ -16,20 +14,24 @@ import { History } from './src/app/service/history.component';
 import { AsciidocIndex } from './src/app/components/asciidoc/asciidoc.index';
 import { AsciidocService } from './src/app/components/asciidoc/asciidoc.service';
 
+// import { SingleSelectionDropDownComponent } from './src/app/comp
+// onents/single-selection-dropdown/single-selection-dropdown.component';
+import { MultipleSelectionListComponent } from './src/app/components/multiple-selection-list/multiple-selection-list.component';
+
+
+
 const classes = [
-  AsciidocComponent,
   ForgeErrorsComponent,
   ForgeExceptionComponent,
   InputComponent,
-  MultipleSelectionListComponent,
   ProjectSelect,
-  SingleSelectionDropDownComponent
+  MultipleSelectionListComponent
 ];
 
 @NgModule({
-  imports: [CommonModule, FormsModule, MultiselectDropdownModule],
-  exports: classes,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MultiselectDropdownModule],
   declarations: classes,
+  exports: classes,
   providers: [
     ForgeService,
     AsciidocIndex,
