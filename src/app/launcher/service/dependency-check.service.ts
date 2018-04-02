471657512c493d612a2109de712b88369864d484
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2018-04-02 20:08:38 +0530

    Changes for full input flow for dependency editor

--- src/app/launcher/service/dependency-check.service.ts
+++ src/app/launcher/service/dependency-check.service.ts
@@ -44,4 +44,5 @@ export abstract class DependencyCheckService {
    * @returns boolean
    */
   abstract validateProjectVersion(projectVersion: string): boolean;
+  abstract getBoosterInfo(missionId: string, runtimeId: string): Observable<string>;
 }
