a19918950bd460d4b8f5fc26724bf08bf41e69ca
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2018-04-02 20:08:38 +0530

    Changes for full input flow for dependency editor

--- src/app/launcher/launcher.module.ts
+++ src/app/launcher/launcher.module.ts
@@ -2,6 +2,7 @@ import { NgModule, Provider } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { DependencyEditorModule } from 'fabric8-analytics-dep-editor';
 import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { PopoverModule } from 'ngx-bootstrap/popover';
 import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
@@ -15,6 +16,7 @@ import { CancelOverlayComponent } from './cancel-overlay/cancel-overlay.componen
 
 import { ActivateBoosterCreateappNextstepComponent }
   from './create-app/activate-booster-createapp-nextstep/activate-booster-createapp-nextstep.component';
+import { DependencyEditorCreateappStepComponent } from './create-app/dependency-editor-step/dependency-editor-step.component';
 import { GitproviderCreateappStepComponent }
   from './create-app/gitprovider-createapp-step/gitprovider-createapp-step.component';
 import { MissionRuntimeCreateappStepComponent }
@@ -52,6 +54,7 @@ export const providers: Provider[] = [
   imports: [
     BsDropdownModule.forRoot(),
     CommonModule,
+    DependencyEditorModule,
     FormsModule,
     InViewportModule.forRoot(providers),
     PipeModule,
@@ -65,6 +68,7 @@ export const providers: Provider[] = [
   declarations: [
     ActivateBoosterCreateappNextstepComponent,
     CancelOverlayComponent,
+    DependencyEditorCreateappStepComponent,
     GitproviderCreateappStepComponent,
     GitproviderImportappStepComponent,
     MissionRuntimeCreateappStepComponent,
